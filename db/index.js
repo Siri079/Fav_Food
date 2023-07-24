9692a0bf7b72a2e8c28e1e33235c4098bc1bc92d
Kamkanampati Siri <93307903+Siri079@users.noreply.github.com>
2023-07-24 17:41:33 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const mongoose=require("mongoose");
 mongoose
-    .connect('mongodb://127.0.0.1:27017/food-app', {useNewUrlParser:true})
+    // .connect('mongodb://127.0.0.1:27017/food-app', {useNewUrlParser:true})
+     .connect('mongodb+srv://admin:l8PwRaDfGMVfNywF@cluster0.fa7awu3.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser:true})
     .catch(e=>{
         console.error('connection error', e.message);
     })
