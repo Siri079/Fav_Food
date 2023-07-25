bf8d98efb3406fa239c7d1079207aa63c7679d2d
Kamkanampati Siri <93307903+Siri079@users.noreply.github.com>
2023-07-25 11:44:37 +0530

    Update index.jsx

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -74,7 +74,7 @@ const Login = () => {
                         className="block appearance-none w-full px-3 py-2 border border-gray-300 roundedn-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-200 focus:border-gray-200"
                         />
                     </div>
-                    <Button variant="secondary" size="large">{loading ? "Loading" : 'Register'}</Button>
+                    <Button variant="secondary" size="large">{loading ? "Loading" : 'LogIn'}</Button>
                 </form>
                 <ToastContainer />
                 </div>
